9a00256b0221bcd0c262298da9fbd277a99cf880
Sondre94 <sondre.f.kodehode@gmail.com>
2022-08-01 09:17:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let inputEl = document.getElementById('input-el')
 let count = 0
 let collatsArr = []
-const max = Math.max(collatsArr)
 
 function submit() {
 
@@ -9,7 +8,7 @@ function submit() {
   let collatz = (number) => {
     while(number != 1){
 
-      if(number % 2 == 0){
+      if(number % 2 === 0){
         collatsArr.push(number)
         number = number / 2; 
       }
@@ -29,7 +28,5 @@ let max = Math.max.apply(0, collatsArr);
     document.getElementById('inputNum').innerHTML = "Utfører Collatz funksjonen på tallet " +  inputEl.value 
     document.getElementById('collatzLength').innerHTML = "Antall steg før tallet endte på 1: " + collatsArr.length
     document.getElementById('collatzMax').innerHTML = "Høyeste tall nådd i sekvensen: " + max
-
-
 
 }
